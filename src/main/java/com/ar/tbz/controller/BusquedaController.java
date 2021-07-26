3bc137e544bd9f4795270680a386e13f282eeff5
Emiliano Veloso <elemudela@gmail.com>
2021-07-26 14:51:57 -0300

    Update

--- src/main/java/com/ar/tbz/controller/BusquedaController.java
+++ src/main/java/com/ar/tbz/controller/BusquedaController.java
@@ -3,24 +3,30 @@ package com.ar.tbz.controller;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ar.tbz.domain.Autodiagnostico;
-import com.ar.tbz.services.Servicios;
+import com.ar.tbz.services.BusquedaService;
 
 @RestController
-@RequestMapping("/busqueda")
-@CrossOrigin(origins = { "http://localhost:4200", "http://34.239.14.244:4200", "http://34.239.14.244",
-		"https://autodiagnosticotest.elea.com:2810", "https://autodiagnostico.elea.com:2809" })
+@RequestMapping("/buscar")
+//@CrossOrigin(origins = { "http://localhost:4200", "http://34.239.14.244:4200", "http://34.239.14.244",
+//		"https://autodiagnosticotest.elea.com:2810", "https://autodiagnostico.elea.com:2809" })
 
 public class BusquedaController {
 
-	@RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
-	public synchronized static List<Autodiagnostico> buscarDiagnostico(Map<String, String> form) throws Exception {
-		return Servicios.buscarDiagnostico(form);
+	@Autowired
+	BusquedaService busquedaService;
+
+	@RequestMapping(value = "/buscar", method = RequestMethod.GET, produces = "application/json")
+	public List<Autodiagnostico> buscarDiagnostico(@RequestParam Map<String, String> form, @RequestParam Integer pagina)
+			throws Exception {
+		System.out.println(pagina);
+		return busquedaService.buscarDiagnostico(form);
 	}
 
 }
