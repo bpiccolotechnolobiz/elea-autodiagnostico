68352436238cecc36a0da606d657049f5b910bc1
Emiliano Veloso <elemudela@gmail.com>
2021-07-26 22:30:41 -0300

    update busqueda controller

--- src/main/java/com/ar/tbz/controller/BusquedaController.java
+++ src/main/java/com/ar/tbz/controller/BusquedaController.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ import com.ar.tbz.domain.Autodiagnostico;
 import com.ar.tbz.services.BusquedaService;
 
 @RestController
-@RequestMapping("/buscar")
+
 //@CrossOrigin(origins = { "http://localhost:4200", "http://34.239.14.244:4200", "http://34.239.14.244",
 //		"https://autodiagnosticotest.elea.com:2810", "https://autodiagnostico.elea.com:2809" })
 
@@ -22,7 +21,7 @@ public class BusquedaController {
 	@Autowired
 	BusquedaService busquedaService;
 
-	@RequestMapping(value = "/buscar", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/buscar", produces = "application/json")
 	public List<Autodiagnostico> buscarDiagnostico(@RequestParam Map<String, String> form, @RequestParam Integer pagina)
 			throws Exception {
 		System.out.println(pagina);
