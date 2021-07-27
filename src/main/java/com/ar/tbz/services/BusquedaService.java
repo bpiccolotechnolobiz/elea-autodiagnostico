6aa5eb034faf29019df1916a1852199a2406003b
Emiliano Veloso <elemudela@gmail.com>
2021-07-27 12:06:37 -0300

    Update prepared statement execute query

--- src/main/java/com/ar/tbz/services/BusquedaService.java
+++ src/main/java/com/ar/tbz/services/BusquedaService.java
@@ -39,7 +39,7 @@ public class BusquedaService {
 			conn = Conexion.generarConexion();
 			log.info("Query_statement: " + query);
 			PreparedStatement pstm = conn.prepareStatement(query);
-			ResultSet rs = pstm.executeQuery(sb.toString());
+			ResultSet rs = pstm.executeQuery();
 			while (rs.next()) {
 				Autodiagnostico nuevoAutoD = new Autodiagnostico(rs.getInt(1), rs.getString(2), rs.getString(3),
 						rs.getString(4), rs.getString(5), rs.getString(6), rs.getInt(9), rs.getBoolean((10)));
