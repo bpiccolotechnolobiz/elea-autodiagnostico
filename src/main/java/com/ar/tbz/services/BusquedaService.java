c04278012ddee8d8f708eb543c5dc3b23909c11a
Emiliano Veloso <elemudela@gmail.com>
2021-07-29 17:29:22 -0300

    Query busqueda: Fecha hasta

--- src/main/java/com/ar/tbz/services/BusquedaService.java
+++ src/main/java/com/ar/tbz/services/BusquedaService.java
@@ -32,9 +32,9 @@ public class BusquedaService {
 					if (entry.getKey().equals("nroLegajo") && entry.getValue().equals("-1")) {
 						sb.append(" and " + entry.getKey() + " > 0 ");
 					} else if (entry.getKey().equals("fecha_autodiagnostico")) {
-						sb.append(" and " + entry.getKey() + " between " + entry.getValue());
+						sb.append(" and " + entry.getKey() + " >= " + entry.getValue());
 					} else if (entry.getKey().equals("fecha_autodiagnostico_hasta")) {
-						sb.append(" and " + entry.getValue());
+						sb.append(" and fecha_autodiagnostico " + " <= " + entry.getValue());
 					} else {
 						sb.append(" and a." + entry.getKey() + " = " + entry.getValue());
 					}
