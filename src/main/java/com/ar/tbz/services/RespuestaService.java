fe17c73c35dab0d6578503afcbba425c320451be
Emiliano Veloso <emiliano.veloso@gmail.com>
2021-10-21 15:27:14 -0300

    Save or update respuesta

--- src/main/java/com/ar/tbz/services/RespuestaService.java
+++ src/main/java/com/ar/tbz/services/RespuestaService.java
@@ -2,6 +2,7 @@ package com.ar.tbz.services;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.stereotype.Service;
@@ -11,9 +12,9 @@ import com.ar.tbz.domain.Respuesta;
 
 @Service
 public class RespuestaService {
-	
+
 	public void insert(Respuesta respuesta) throws SQLException {
-		
+
 		Connection conn = null;
 		PreparedStatement pstm = null;
 
@@ -28,9 +29,9 @@ public class RespuestaService {
 		pstm.setString(5, respuesta.getTextoPregunta());
 		pstm.executeUpdate();
 		conn.close();
-		
+
 	}
-	
+
 	public void update(Respuesta respuesta) throws SQLException {
 
 		Connection conn = null;
@@ -71,13 +72,52 @@ public class RespuestaService {
 		PreparedStatement pstm = null;
 
 		conn = Conexion.generarConexion();
-		String update = "UPDATE respuestasPerfilEmpleado SET respuestaPregunta = ? where nroLegajo = ? and idPregunta = ?";
-		pstm = conn.prepareStatement(update);
-		pstm.setString(1, respuesta.getRespuestaPregunta());
-		pstm.setInt(2, respuesta.getIdPregunta());
-		pstm.setString(3, nroLegajo);
+
+		Respuesta resDB = findById(respuesta.getId());
+
+		if (resDB == null) {
+
+			String insert = "INSERT INTO respuestasPerfilEmpleado(nroLegajo, idPregunta, respuestaPregunta) values"
+					+ "(?, ?, ?)";
+			pstm = conn.prepareStatement(insert);
+			pstm.setString(1, nroLegajo);
+			pstm.setInt(2, respuesta.getIdPregunta());
+			pstm.setString(3, respuesta.getRespuestaPregunta());
+
+		} else {
+
+			String update = "UPDATE respuestasPerfilEmpleado SET respuestaPregunta = ? where nroLegajo = ? and idPregunta = ?";
+			pstm = conn.prepareStatement(update);
+			pstm.setString(1, respuesta.getRespuestaPregunta());
+			pstm.setInt(2, respuesta.getIdPregunta());
+			pstm.setString(3, nroLegajo);
+		}
+
 		pstm.executeUpdate();
 		conn.close();
+	}
+
+	public Respuesta findById(int id) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstm = null;
+
+		conn = Conexion.generarConexion();
+		String findById = "select * from respuesta where id = ?";
+		pstm = conn.prepareStatement(findById);
+		pstm.setInt(1, id);
+		ResultSet rs = pstm.executeQuery();
+		Respuesta res = null;
+		if (rs.next()) {
+			res = new Respuesta();
+			res.setId(rs.getInt("id"));
+			res.setIdAutodiagnostico(rs.getInt("idAutodiagnostico"));
+			res.setIdPregunta(rs.getInt("idPregunta"));
+			res.setRespuestaPregunta(rs.getString("respuestaPregunta"));
+			res.setTextoPregunta(rs.getString("textoPregunta"));
+			res.setVersion(rs.getInt("version"));
+		}
+		conn.close();
+		return res;
 	}
 
 }
